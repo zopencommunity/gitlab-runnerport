66c7911830719cd2b7582d283907089d5bdcb56e
jlee3227 <joon.lee3227@gmail.com>
2023-12-13 21:46:32 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitLab Runner
+
+GitLab Runner is the open source project that is used to run your CI/CD jobs and send the results back to GitLab
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gitlab-runnerport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gitlab-runnerport.git'), string(name: 'PORT_DESCRIPTION', value: 'GitLab Runner' ), string(name: 'BUILD_LINE', value: 'DEV'), string(name: 'NODE_LABEL', value: "go_120" )  ]
+  }
+}
