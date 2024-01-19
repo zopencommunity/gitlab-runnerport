889817dcc1d760bd5241a4703680d1c936b6de4f
v1gnesh <v1gnesh@users.noreply.github.com>
2024-01-19 06:49:15 +0530

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -9,6 +9,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gitlab-runnerport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gitlab-runnerport.git'), string(name: 'PORT_DESCRIPTION', value: 'GitLab Runner is a tool that allows you to run your CI/CD jobs and send the results back to GitLab.' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gitlab-runnerport.git'), string(name: 'PORT_DESCRIPTION', value: 'GitLab Runner is a tool that allows you to run your CI/CD jobs and send the results back to GitLab.' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "go_120" ) ]
   }
 }
